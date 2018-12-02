148c5c1d704d1b128bd1e16a21cb5a37a6fb2728
Christophe '116' Loiseau <116@lab0.net>
2018-12-02 12:37:06 +0100

    Day2

---
+++ 2018/src/main/kotlin/pack/d2.kt
@@ -0,0 +1,48 @@
+package pack
+
+import com.google.common.collect.Lists
+import com.google.common.io.Resources.getResource
+import com.google.common.io.Resources.toString
+import org.funktionale.currying.curried
+import java.nio.charset.StandardCharsets.UTF_8
+
+fun main(args: Array<String>) {
+  val txt = toString(getResource("2.input.txt"), UTF_8).split("\n")
+
+  /*
+   * Keeps words which have at least 1 repetition of exactly n letters
+   */
+  val dispatcher = { n: Int, txt: List<String> ->
+    txt.filter { word ->
+      word.groupBy {
+        it
+      }.any {
+        it.value.size == n
+      }
+    }
+  }.curried()
+
+  val couples = dispatcher(2)(txt)
+  val triples = dispatcher(3)(txt)
+
+  println(couples.size * triples.size)
+
+  // PART2
+  val diff = { s: String, t: String ->
+    if (s.length != t.length) -1
+    else s.zip(t).count {
+      it.first != it.second
+    }
+  }
+
+  val oneCharDiff = Lists.cartesianProduct(txt, txt).filter {
+    diff(it[0],it[1]) == 1
+  }
+
+  val pair = oneCharDiff.first() // assuming there is only 1 instance of such a combination
+  val code = pair[0].zip(pair[1]).joinToString(separator = "") {
+    if(it.first == it.second) it.first.toString() else ""
+  }
+
+  println(code)
+}
