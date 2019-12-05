b0a1dd5a7339304cbe1d1675cc43e120f4e69693
Christophe '116' Loiseau <116@lab0.net>
2019-12-05 20:00:39 +0100

    D4B

--- 2019/src/main/kotlin/y2019/D4.kt
+++ 2019/src/main/kotlin/y2019/D4.kt
@@ -14,3 +14,22 @@ object D4A {
         it.toString().zipWithNext().all { it.first <= it.second }
       }
 }
+
+object D4B {
+  fun solve(range: IntRange) =
+      // in range
+      range.filter {
+        // has length 6
+        it.toString().length == 6
+      }.filter {
+        // has 2 identical elements 1 next to the other
+        it.toString().zipWithNext().any { it.first == it.second }
+      }.filter {
+        // never decreases
+        it.toString().zipWithNext().all { it.first <= it.second }
+      }.filter {
+        // the two adjacent matching digits are not part of a larger group of matching digits
+        it.toString().groupBy { it }.values.groupBy { it.size }[2]?.size ?: 0 >= 1
+      }
+
+}
---
+++ 2019/src/test/kotlin/y2019/D4BTest.kt
@@ -0,0 +1,13 @@
+package y2019
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class D4BTest {
+  @Test
+  fun `solve`() {
+      println(
+          D4B.solve(273025..767253).size
+      )
+  }
+}
